9d4c668c3cc496ad8f426a9b83ac9037bc563f74
John Caldwell <john.caldwell@caldwelldevelopment.com>
2024-06-10 07:09:04 -0400

    Added background and started a header

---
+++ johncaldwell_myprofile/src/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+    
+    return (
+        <div className="headerTitle">
+            <h1>Hello Everyone, My name is John Caldwell</h1>
+        </div>
+    );
+    
+}
+
+export default Header
