bffa079b8e8b076284fb0abe5a1690a3110c0e11
John Caldwell <john.caldwell@caldwelldevelopment.com>
2024-06-12 06:25:47 -0400

    Removed border from CSS, changed title, and added jpg to uses for Favicon.

--- johncaldwell_myprofile/src/Header.jsx
+++ johncaldwell_myprofile/src/Header.jsx
@@ -1,10 +1,23 @@
-
+import selfImage from './assets/selfie-01.jpg'
 function Header() {
     
+    const quotes = ["If you want to achieve greatness stop asking for permission. ~Anonymous]",
+        "Things work out best for those who make the best of how things work out. ~John Wooden",
+        "To live a creative life, we must lose our fear of being wrong. ~Anonymous}",
+        "If you are not willing to risk the usual you will have to settle for the ordinary. ~Jim Rohn"]
     return (
+    <>
         <div className="headerTitle">
-            <h1>Hello Everyone, My name is John Caldwell</h1>
+                <div className='title'>
+                    <h1>Hello Everyone, My name is John Caldwell</h1>
+                    <h3 id="quotes">{quotes[3]}</h3>
+                </div>
+                <div className='title-img'>
+                    <img className="mySelfImage" src={selfImage} height={250} width={250} alt="My Picture"></img>
+                </div>
         </div>
+    </>
+
     );
     
 }
